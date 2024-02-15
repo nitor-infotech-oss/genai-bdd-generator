77abbdd0db3512ff51e1550627d0b1da234b820b
Shalu Barman <shalu.barman@NIT-MAC-U2-2023-003.local>
2024-02-15 21:49:19 +0530

    updated requirements file

--- services/evaluator.py
+++ services/evaluator.py
@@ -1,30 +1,22 @@
 from dotenv import load_dotenv
+
 load_dotenv()
-
-import os
-
+from langchain_openai import ChatOpenAI
 from datasets import Dataset
-import google.generativeai as genai
 from ragas import evaluate
-from ragas.metrics import faithfulness, answer_relevancy, context_precision
+# from ragas.metrics import faithfulness, answer_relevancy, context_precision
+from ragas.metrics import (faithfulness, answer_relevancy,
+            context_precision, context_relevancy, 
+            context_recall, answer_similarity, 
+            answer_correctness)
 from ragas.llms import LangchainLLM
-from langchain_google_genai import ChatGoogleGenerativeAI
 
-from services.ai_model import get_llm_response
-from services.document import DocumentHelper
+from .ai_model import get_llm_response
+from .document import DocumentHelper
 from constants import prompt_template, questions
 
-# from langchain.llms import Ollama
-# from langchain.llms import OpenAI
-# from langchain_openai import OpenAI
-# from langchain.chat_models import ChatOpenAI
-
-
-# llm = ChatOpenAI(model_name="gpt-3.5-turbo")
+llm = ChatOpenAI(model_name="gpt-3.5-turbo")
 # llm = Ollama(model="llama2", temperature=0)
-genai.configure(api_key=os.environ.get("GOOGLE_API_KEY"))
-# llm = genai.GenerativeModel(model_name = "gemini-pro")
-llm = ChatGoogleGenerativeAI(model="gemini-pro")
 vllm = LangchainLLM(llm=llm)
 faithfulness.llm = vllm
 answer_relevancy.llm = vllm
@@ -40,42 +32,25 @@ for query in questions:
     answers.append(get_llm_response(prompt=prompt))
     contexts.append([doc.page_content for doc in earlier_docs])
 
-# # To dict
-data = {
-        "question": prompts,
-        "answer": answers,
-        "contexts": contexts,
-     }
-data2 = {
-        "question": questions,
-        "answer": answers,
-        "contexts": contexts,
-     }
+data = {"question": prompts, "answer": answers}
+if contexts:
+    data["contexts"] = contexts
 
-
-# Convert dict to dataset
 dataset = Dataset.from_dict(data)
-dataset2 = Dataset.from_dict(data2)
 
-firstresult = evaluate(
+
+result = evaluate(
     dataset = dataset, 
     metrics=[
         context_precision,
         faithfulness,
         answer_relevancy,
+        context_relevancy,
+        context_recall,
+        answer_similarity,
+        answer_correctness
             ])
 
-secondresult = evaluate(
-    dataset = dataset2, 
-    metrics=[
-        context_precision,
-        faithfulness,
-        answer_relevancy,
-            ])
-
-print("first...")
-print(firstresult)
-print("second...")
-print(secondresult)
-
+print("Result is...")
+print(result)
 
